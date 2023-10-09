41bfd5a7456794c357a61d746ce30a12c860ed9b
LDjandj42 <ldumas@jnjdevelopment.fr>
2023-10-09 16:58:06 +0200

    09/10/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, map, withLatestFrom } from 'rxjs';
+import { Character } from './starwars/characters';
+import { Species } from './starwars/interface';
+import { loadCharacterList } from './state-characters/actions';
+import { getCharctersList } from './state-characters/characters-selectors';
+import { loadSpeciesList } from './state-species/actions';
+import { getSpeciesList } from './state-species/species-selectors';
 
 
 
@@ -13,15 +21,37 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title(title: any) {
     throw new Error('Method not implemented.');
+    
   }
 
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-    }
   
+
+  charactersList$: Observable<Character[]>
+  speciesList$: Observable<Species[]>
+  constructor(private store: Store) { }
+
+  ngOnInit() {
+    this.store.dispatch(loadSpeciesList());
+    this.store.dispatch(loadCharacterList());
+    this.charactersList$ = this.store.select(getCharctersList).pipe(
+      withLatestFrom(this.store.select(getSpeciesList)),
+      map((([characterList, speciesList]: [Character[],Species[]]):Character[] => {
+        const characterListToReturn:Character[]= characterList.map((character: Character) => {
+          const speciesUrl:string = character.species[0];
+          const matchingSpecies:Species = speciesList.find((species: Species) => species.url === speciesUrl);
+          if (character.species.length === 0) { //this logic is because characterList can be empty
+            return { ...character, speciesName: 'Human' };
+          };
+          return { ...character, speciesName: matchingSpecies?.name };
+          
+        });
+        return characterListToReturn;
+      }))
+      );
+
+    
+  }
   }
   
 
